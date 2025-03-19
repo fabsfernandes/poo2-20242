b9620378b05de9b4601522c3b200b450e13123c4
Fabiola Souza Fernandes Pereira <fabs177@hotmail.com>
2025-03-19 06:08:56 -0300

    Add files via upload

---
+++ lab1/singleresponsibility/correct/BookPrinter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singleresponsibility.correct;
+
+/**
+ *
+ * @author fabiola
+ */
+public class BookPrinter {
+    
+       
+    public void printAuthorName(String author) {
+        System.out.println("Nome do autor -> "+ author);
+    }
+    
+}
